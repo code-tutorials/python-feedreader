5caccf8a87cd1e09a9f2ebb769e494b4155410d2
davy <davy.wybiral@gmail.com>
2017-12-01 11:04:16 -0600

    Added update loop

--- run.py
+++ run.py
@@ -2,8 +2,31 @@ from app import app
 from db import db
 from models import article, source
 import routes
+import feed
+from threading import Thread
+import time
 
 with app.app_context():
     db.create_all()
+
+def update_loop():
+    while True:
+        with app.app_context():
+            query = source.Source.query
+            for src in query.all():
+                try:
+                    update_source(src)
+                except:
+                    continue
+        time.sleep(60 * 15)
+
+def update_source(src):
+    parsed = feed.parse(src.feed)
+    feed_articles = feed.get_articles(parsed)
+    article.Article.insert_from_feed(src.id, feed_articles)
+    print('Updated ' + src.feed)
+
+thread = Thread(target=update_loop)
+thread.start()
 
 app.run()
